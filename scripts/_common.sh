a718380bfd6982d2a87c71f1b67de27ebe72b910
Boudewijn <wbk_git@online.osba.nl>
2025-03-03 15:25:23 +0100

    retry, now generating  in _common instead of manifest

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,3 +5,5 @@
 #=================================================
 composer_version="2.5.5"
 php_version="8.3"
+# will not stay stable over installs, should not be a problem
+token=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 60)
